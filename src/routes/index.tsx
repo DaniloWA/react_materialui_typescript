bbd3fee4cdea8ebbdd9eecaf48cdb5f5b0ebd817
Danilo <daniloworkdev@gmail.com>
2022-04-16 19:52:11 +0100

    feat: Adicionado react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export { };
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+
+    return(
+        <Routes>
+            <Route path="/"element={<p>Pagina inicial</p>} />
+
+            <Route path="*"element={<Navigate to="/"/>} />
+        </Routes>
+    );
+}
